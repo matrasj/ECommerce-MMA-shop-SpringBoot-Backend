39e390bb7aa4c41b75dc2d63a9bc718577353058
pozerinhooo <jkob.matras@gmail.com>
2022-09-16 11:06:20 +0200

    Third try :-(

--- src/main/java/com/example/ecommercebackend/service/EmailSenderService.java
+++ src/main/java/com/example/ecommercebackend/service/EmailSenderService.java
@@ -12,19 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailSenderService {
-    private final JavaMailSender javaMailSender;
-    private final AppConfig appConfig;
+//    private final JavaMailSender javaMailSender;
+//    private final AppConfig appConfig;
 
     @Async
     public void sendConfirmationEmail(User user, ConfirmationToken token) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("mma.shop@gmail.com");
-        simpleMailMessage.setTo(user.getEmail());
-        simpleMailMessage.setSubject("Email confirmation");
-        simpleMailMessage.setText("Hi, " + user.getUsername() + "\nClick here to confirm your email: "
-        + appConfig.getUrl() + "/api/v1/auth/confirmation?token=" + token.getToken());
-
-        javaMailSender.send(simpleMailMessage);
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setFrom("mma.shop@gmail.com");
+//        simpleMailMessage.setTo(user.getEmail());
+//        simpleMailMessage.setSubject("Email confirmation");
+//        simpleMailMessage.setText("Hi, " + user.getUsername() + "\nClick here to confirm your email: "
+//        + appConfig.getUrl() + "/api/v1/auth/confirmation?token=" + token.getToken());
+//
+//        javaMailSender.send(simpleMailMessage);
 
     }
 }
