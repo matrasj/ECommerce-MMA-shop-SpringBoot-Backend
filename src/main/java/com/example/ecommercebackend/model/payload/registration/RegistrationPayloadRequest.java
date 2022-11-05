23b41faf3f310cd9577b93de309fc593c97c7586
pozerinhooo <jkob.matras@gmail.com>
2022-11-05 19:41:22 +0100

    Added size validation for username and password

--- src/main/java/com/example/ecommercebackend/model/payload/registration/RegistrationPayloadRequest.java
+++ src/main/java/com/example/ecommercebackend/model/payload/registration/RegistrationPayloadRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,9 +22,11 @@ public class RegistrationPayloadRequest {
     private String lastName;
 
     @NotBlank(message = "Username can not be null")
+    @Size(min = 4, max = 25, message = "Username must be longer than 4 and less than 25 characters")
     private String username;
 
     @NotBlank(message = "Password can not be null")
+    @Size(min = 4, max = 25, message = "Password must be longer than 4 and less than 25 characters")
     private String password;
 
     @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$", message = "Email must be valid")
